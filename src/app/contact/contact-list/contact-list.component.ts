e5a0f9912f66901ee8fa130ed904f21e584fcd67
TuomasVille <tuomas_kayhty@hotmail.com>
2018-05-16 12:10:44 +0300

    Added contact service, synkronous implemention.

--- src/app/contact/contact-list/contact-list.component.ts
+++ src/app/contact/contact-list/contact-list.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {Contact} from '../contact';
+import {ContactService} from '../contact.service';
 
 @Component({
   selector: 'cw-contact-list',
@@ -10,15 +11,11 @@ export class ContactListComponent implements OnInit {
 
   contacts: Contact[];
 
-  constructor() {
+  constructor(private contactService: ContactService) {
     this.contacts = [];
   }
 
   ngOnInit() {
-    this.contacts.push(new Contact('Seppo', 'Taalasmaa'));
-    this.contacts.push(new Contact('Jorma', 'Jokinen'));
-    this.contacts.push(new Contact('Keijo', 'Kuningas'));
-    console.log(this.contacts);
+    this.contacts = this.contactService.getContacts();
   }
-
 }
