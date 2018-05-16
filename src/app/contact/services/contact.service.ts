9292c390ed91aaf38f4a45f2ba849a157c3d1290
TuomasVille <tuomas_kayhty@hotmail.com>
2018-05-16 13:11:52 +0300

    Added simple test API interface implemention, Added contactService, getting contacts from a server is supported.

--- src/app/contact/services/contact.service.ts
+++ src/app/contact/services/contact.service.ts
@@ -1,21 +1,31 @@
 import {Injectable} from '@angular/core';
 import {Contact} from '../contact';
+import {Observable} from 'rxjs';
+import {ContactHttpService} from './contact-http.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ContactService {
 
-  contacts: Contact[];
+  // contacts: Contact[];
 
-  constructor() {
-    this.contacts = [];
-    this.contacts.push(new Contact(1, 'Seppo', 'Taalasmaa'));
-    this.contacts.push(new Contact(2, 'Jorma', 'Jokinen'));
-    this.contacts.push(new Contact(3, 'Keijo', 'Kuningas'));
+  constructor(private contactHttpService: ContactHttpService) {
+    /*
+    constructor() {
+      this.contacts = [];
+      this.contacts.push(new Contact(1, 'Seppo', 'Taalasmaa'));
+      this.contacts.push(new Contact(2, 'Jorma', 'Jokinen'));
+      this.contacts.push(new Contact(3, 'Keijo', 'Kuningas'));
+      */
   }
 
+  /*
   getContacts(): Contact[] {
     return this.contacts;
+  }
+  */
+  getContacts(): Observable<Contact[]> {
+    return this.contactHttpService.get();
   }
 }
