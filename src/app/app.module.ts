7ed59c96caf77fc8f4e6c544dc3bd19a2fd5e3b7
TuomasVille <tuomas_kayhty@hotmail.com>
2018-05-18 11:11:47 +0300

    Added sidenav layout and toolbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,9 @@ import {ContactHttpService} from './contact/services/contact-http.service';
 import {HttpClientModule} from '@angular/common/http';
 import { ContactDetailComponent } from './contact/contact-detail/contact-detail.component';
 import {RouterModule, Routes} from '@angular/router';
+import {MatButtonModule} from '@angular/material';
+import {MaterialComponentsModule} from './ui/material-components/material-components.module';
+import {FlexLayoutModule} from '@angular/flex-layout';
 
 const appRoutes: Routes = [
   {path: 'contacts', component: ContactListComponent},
@@ -26,7 +29,9 @@ const appRoutes: Routes = [
     BrowserModule,
     FormsModule,
     HttpClientModule,
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes),
+    MaterialComponentsModule,
+    FlexLayoutModule
   ],
   providers: [
     ContactService,
