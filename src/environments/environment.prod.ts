7ab36a4504888fe92105bf9f978ae46a9b8d33ca
TuomasVille <tuomas_kayhty@hotmail.com>
2019-05-16 14:37:24 +0300

    Initial commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  production: false,
+  production: true,
   apiEndpointUrl: 'https://contactswebapitk.azurewebsites.net'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: true,
-  apiEndpointUrl: 'https://contactswebapi.azurewebsites.net/api/contacts'
+  production: false,
+  apiEndpointUrl: 'https://contactswebapitk.azurewebsites.net'
 };
 
 /*
